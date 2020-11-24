3e3cde25c8bfd2f7968b60c72d3d6cb0752b66eb
Jordan Moss <jmoss636@gmail.com>
2020-11-24 23:02:56 +0000

    Test

--- seeds.js
+++ seeds.js
@@ -7,26 +7,6 @@ function seedDB (){
         if(err) {
             throw err;
         }
-        
-        // data.forEach(seed => Campground.create(seed, (err, campground) => {
-        //     if(err) {
-        //         throw err;
-        //     }
-        //     Comment.create(
-        //         {
-        //             text: 'This place is great, but I wish there was internet!',
-        //             author: 'Homer'
-        //         },
-        //         (err, comment) => {
-        //             if(err){
-        //                 throw err;
-        //             } else {
-        //             campground.comments.push(comment)
-        //             campground.save()
-        //             }
-        //         })
-        //     })
-        // )
     })
     User.remove({}, err => {
         if(err) {
