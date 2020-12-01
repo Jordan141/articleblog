8a52023f0b45a4942dd802fcadb24cf40b085e4d
Jordan Moss <jmoss636@gmail.com>
2020-12-01 20:51:12 +0000

    fix small csp errors

--- app/app.js
+++ app/app.js
@@ -96,11 +96,11 @@ app.use(helmet.ieNoOpen())
 app.use(helmet.hidePoweredBy({setTo: 'Whisky Powered.'}))
 app.use(helmet.contentSecurityPolicy({
     directives: {
-        defaultSrc: ["'self'", "https://maxcdn.bootstrapcdn.com"],  // default value for all directives that are absent
-        scriptSrc: ["'self'", "https://cdnjs.cloudflare.com"],   // helps prevent XSS attacks
+        defaultSrc: ["'self'", "https://stackpath.bootstrapcdn.com"],  // default value for all directives that are absent
+        scriptSrc: ["'self'", "https://code.jquery.com/", "https://stackpath.bootstrapcdn.com"],   // helps prevent XSS attacks
         frameAncestors: ["'none'"],  // helps prevent Clickjacking attacks
-        styleSrc: ["https://maxcdn.bootstrapcdn.com", "'self'" ],
-        imgSrc: ["'self'"]
+        styleSrc: ["https://stackpath.bootstrapcdn.com", "'self'" ],
+        imgSrc: ["'self'", "http://i.imgur.com"]
     }
 }))
 
