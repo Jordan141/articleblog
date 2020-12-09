98d61925de17e4adc73740e3b33eb3008851de38
Jordan Moss <jmoss636@gmail.com>
2020-12-09 06:18:07 +0000

    dynamically generate sitemap

--- app/app.js
+++ app/app.js
@@ -14,7 +14,8 @@ const express           = require('express'),
       tooBusy           = require('toobusy-js'),
       User              = require('./models/user'),
       helmet            = require('helmet'),
-      rateLimit         = require('express-rate-limit')
+      rateLimit         = require('express-rate-limit'),
+      sitemap           = require('express-sitemap')()
 
 const db = {
     name: process.env.MONGO_INITDB_DATABASE,
@@ -135,4 +136,5 @@ app.use('/articles', articleRoutes)
 app.use('/articles/:id/comments', commentRoutes)
 app.locals.moment = require('moment')
 
+console.log(sitemap.generate4(app, [, articleRoutes]))
 app.listen(PORT, IP, () => console.log(`Server is listening on ${IP}:${PORT}`))
