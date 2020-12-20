f4571ec71a9657d910272dbab71d4c08d5a2bb8c
Jordan Moss <jmoss636@gmail.com>
2020-12-20 19:15:43 +0000

    edit profile and non-clickable image bugs

--- app/public/scripts/imageClicker.js
+++ app/public/scripts/imageClicker.js
@@ -2,6 +2,7 @@ window.onload = (e) => {
     const elements = document.querySelectorAll('.cursor-change')
     elements.forEach(element => {
         const data = element.getAttribute("__data") || null
+        console.log(data)
         if(!data) return
         const url =  "/articles/" + data
         element.addEventListener("click", () => {
