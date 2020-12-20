683a8a42cc5538483198e9a81d17e94f7cb7e667
Jordan Moss <jmoss636@gmail.com>
2020-12-20 19:37:39 +0000

    remove console log

--- app/public/scripts/imageClicker.js
+++ app/public/scripts/imageClicker.js
@@ -2,7 +2,6 @@ window.onload = (e) => {
     const elements = document.querySelectorAll('.cursor-change')
     elements.forEach(element => {
         const data = element.getAttribute("__data") || null
-        console.log(data)
         if(!data) return
         const url =  "/articles/" + data
         element.addEventListener("click", () => {
