511b2e0431c1d0ccf33ef0d0c737757039ff1d88
michaelKurowski <Byamarro@gmail.com>
2020-12-21 17:25:28 +0100

    displaying dates

--- app/public/scripts/formatDate.js
+++ app/public/scripts/formatDate.js
@@ -1,7 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('[js-format-date]').forEach(element => {
-    const dateObject = Date.parse(element.innerText)
-    console.log(dateObject)
-    element.innerText = dateObject.toLocaleString('en-GB', { timeZone: 'UTC' })
+    const date = moment(parseInt(element.innerText))
+    const dateToDisplay = mapDateToDisplay(date)
+    element.innerText = dateToDisplay
   })
+
+  function mapDateToDisplay(date) {
+    if (isToday(date)) return date.format('[Today] hh:mm')
+    if (isYesterday(date)) return date.format('[Yesterday] hh:mm')
+    return date.local().format('DD MMMM YYYY')
+  }
+
+  function isToday(momentDate) {
+    const reference = moment(); 
+    const today = reference.clone().startOf('day');
+      return momentDate.isSame(today, 'd');
+  }
+  function isYesterday(momentDate) {
+    const reference = moment(); 
+    const yesterday = reference.clone().subtract(1, 'days').startOf('day');
+      return momentDate.isSame(yesterday, 'd');
+  }
 })
+
