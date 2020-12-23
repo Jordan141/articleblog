ebac43070df3a93e935f0e4417f4f4409086cfef
Jordan Moss <jmoss636@gmail.com>
2020-12-23 05:14:02 +0000

    add Article schema min max validations

--- app/models/article.js
+++ app/models/article.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose')
+const ARTICLE_BODY_MIN_LENGTH = 30,
+ARTICLE_BODY_MAX_LENGTH = 10000,
+ARTICLE_TITLE_MIN_LENGTH = 10,
+ARTICLE_TITLE_MAX_LENGTH = 60,
+ARTICLE_DESC_MIN_LENGTH = 30,
+ARTICLE_DESC_MAX_LENGTH = 2000
+
 
 const articleSchema = new mongoose.Schema({
     author: {
@@ -9,9 +16,9 @@ const articleSchema = new mongoose.Schema({
         username: String
     },
     createdAt: {type: Number, default: +Date.now(), required: true},
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    body: {type: String, required: true},
+    title: {type: String, required: true, minlength: ARTICLE_TITLE_MIN_LENGTH, maxlength: ARTICLE_TITLE_MAX_LENGTH},
+    description: {type: String, required: true, minlength: ARTICLE_DESC_MIN_LENGTH, maxlength: ARTICLE_DESC_MAX_LENGTH},
+    body: {type: String, required: true, minlength: ARTICLE_BODY_MIN_LENGTH, maxlength: ARTICLE_BODY_MAX_LENGTH},
     isApproved: {type: Boolean, default: false, required: true},
     categories: {
         type: [String],
