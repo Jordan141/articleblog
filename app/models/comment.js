4cab206ce5831ae6f867c2311018cc5c467fe671
Jordan Moss <jmoss636@gmail.com>
2020-12-23 05:32:45 +0000

    comment schema lengths

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const {COMMENT_BODY_MIN_LENGTH, COMMENT_BODY_MAX_LENGTH} = JSON.parse(require('fs').readFileSync(require('path').join(__dirname, '../staticdata/minmax.json'), 'utf-8'))
 
 const commentSchema = mongoose.Schema({
-    text: String,
+    text: {type: String, minLength: COMMENT_BODY_MIN_LENGTH, maxLength: COMMENT_BODY_MAX_LENGTH},
     createdAt: {type: Number, default: +Date.now(), required: true},
     author: {
      id: {
