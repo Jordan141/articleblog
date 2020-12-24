bdf7174d6fbadb91cc7af3b7ae006bc72b4e4d10
Jordan Moss <jmoss636@gmail.com>
2020-12-24 11:46:03 +0000

    hash fingerprint

--- app/routes/analytics.js
+++ app/routes/analytics.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const Counter = require('../models/routeCounter')
+const crypto = require('crypto')
 
 router.post('/fingerprint', async (req, res) => {
     const {currentUrl, fingerprint} = req.body
     if(!currentUrl || !fingerprint || !isBase64(fingerprint)) return res.sendStatus(422) //Unprocessable Entity aka bad parameters
-    
+   
     try {
+        const hashedFingerprint = hashFingerprint(fingerprint)
         const route = await Counter.findOne({url: currentUrl})
         if(!route) {
-            await Counter.create({ url: currentUrl, viewCount: 1, visitedUsers: [fingerprint]})
+            await Counter.create({ url: currentUrl, viewCount: 1, visitedUsers: [hashedFingerprint]})
             return res.sendStatus(200)
         }
 
-        const hasUserViewedRoute = route.visitedUsers.find(user => fingerprint === user)
+        const hasUserViewedRoute = route.visitedUsers.find(user => hashedFingerprint === user)
         if(hasUserViewedRoute) return res.sendStatus(200)
         
-        route.visitedUsers.push(fingerprint)
+        route.visitedUsers.push(hashedFingerprint)
         route.viewCount++
         route.save({validateBeforeSave: true, validateModifiedOnly: true})
         return res.sendStatus(200)
@@ -30,5 +32,10 @@ router.post('/fingerprint', async (req, res) => {
 function isBase64(str) {
     //Base64 Length is always divisble by 4
     return str.length % 4 == 0 && /^[A-Za-z0-9+/]+[=]{0,3}$/.test(str)
+}
+
+function hashFingerprint(fingerprint) {
+    const hashedFingerprint = crypto.createHash('sha256').update(fingerprint).digest('hex')
+    return hashedFingerprint
 }
 module.exports = router
