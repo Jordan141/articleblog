212fe75e219b574c5eb1d721880e18a265954c6e
michaelKurowski <Byamarro@gmail.com>
2020-12-25 22:15:02 +0100

    old screen flag retrieval

--- devops/start.sh
+++ devops/start.sh
@@ -1,2 +1,2 @@
  #!/bin/sh
-screen -m bash -c "webhook -hooks hooks.json -verbose >> ./autodeploy.log" >> screen.log
+screen -m -d bash -c "webhook -hooks hooks.json -verbose >> ./autodeploy.log" >> screen.log
