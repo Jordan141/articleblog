6d9935cd9485942146d4243ed2a4af6375aa0435
michaelKurowski <Byamarro@gmail.com>
2020-12-25 22:13:24 +0100

    logging improvements

--- devops/clear-mongodb.sh
+++ devops/clear-mongodb.sh
@@ -1,4 +1,7 @@
- #!/bin/sh
+#!/bin/sh
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>clean-mongodb.out 2>&1
 docker-compose down
 rm -rf ./db
 docker-compose up
--- devops/redeploy.sh
+++ devops/redeploy.sh
@@ -1,4 +1,7 @@
- #!/bin/sh
+#!/bin/sh
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>redeploy.out 2>&1
 docker-compose down
 git stash save "redeploy stash"
 git fetch
