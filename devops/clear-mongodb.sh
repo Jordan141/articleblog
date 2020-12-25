20210334e03f48322c7040bee5fe3815a5d011e3
michaelKurowski <Byamarro@gmail.com>
2020-12-25 22:13:57 +0100

    outputing to file

--- devops/clear-mongodb.sh
+++ devops/clear-mongodb.sh
@@ -2,6 +2,6 @@
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>clean-mongodb.out 2>&1
-docker-compose down
-rm -rf ./db
-docker-compose up
+docker-compose down >&3
+rm -rf ./db >&3
+docker-compose up >&3
--- devops/redeploy.sh
+++ devops/redeploy.sh
@@ -2,10 +2,10 @@
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>redeploy.out 2>&1
-docker-compose down
-git stash save "redeploy stash"
-git fetch
-git checkout origin/master
-docker-compose rm web
-docker-compose build web
-docker-compose up
+docker-compose down >&3
+git stash save "redeploy stash" >&3
+git fetch >&3
+git checkout origin/master >&3
+docker-compose rm web >&3
+docker-compose build web >&3
+docker-compose up >&3
