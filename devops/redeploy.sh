f96814b0391074e620dd251b7635259862422370
michaelKurowski <Byamarro@gmail.com>
2020-12-25 22:20:33 +0100

    fixed capturing output to bash var

--- devops/redeploy.sh
+++ devops/redeploy.sh
@@ -2,7 +2,7 @@
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>redeploy.out 2>&1
-REPO_DIR=git rev-parse --show-toplevel
+REPO_DIR=$(git rev-parse --show-toplevel)
 cd "${REPO_DIR}/"
 docker-compose down >&3
 git stash save "redeploy stash" >&3
