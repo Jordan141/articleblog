c35ead1770f9bad15535a6efd137f479d68fec25
Jordan Moss <jmoss636@gmail.com>
2020-12-31 01:27:59 +0000

    momentify verify schema

--- app/models/verify.js
+++ app/models/verify.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const moment = require('moment')
 
 
 const verifySchema = new mongoose.Schema({
     token: {type: String, required: true, unique: true},
     userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    expiresAt: {type: Date, default: new Date(new Date().setDate(new Date().getDate() + 3))}
+    expiresAt: {type: Date, default: moment().add(3, 'days').valueOf()}
 })
 
 module.exports = mongoose.model('Verify', verifySchema)
