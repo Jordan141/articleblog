fefafa423f6d7773deada2d2a8b98dda5cb82b0f
Jordan Moss <jmoss636@gmail.com>
2021-01-11 18:10:45 +0000

    change log file locations

--- devops/clear-mongodb.sh
+++ devops/clear-mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>clean-mongodb.out 2>&1
+exec 1>logs/clean-mongodb.out 2>&1
 REPO_DIR=$(git rev-parse --show-toplevel)
 cd "${REPO_DIR}/"
 docker-compose down >&3
--- devops/redeploy.sh
+++ devops/redeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>redeploy.out 2>&1
+exec 1>logs/redeploy.out 2>&1
 REPO_DIR=$(git rev-parse --show-toplevel)
 cd "${REPO_DIR}/"
 docker-compose down >&3
