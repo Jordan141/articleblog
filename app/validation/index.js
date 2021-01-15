215b609aed2cbe42cf1d04d8511add4a9da085aa
Jordan Moss <jmoss636@gmail.com>
2021-01-15 00:16:07 +0000

    little bit more adv middleware

--- app/validation/index.js
+++ app/validation/index.js
@@ -1,10 +1,27 @@
-const schemas = require('./schemas')
+const validationSchemas = require('./schemas')
+const logger = require('../logger')
+const GET = 'GET', PARAMS = 'params', QUERY = 'query', BODY = 'body'
+const QUERY_ROUTES = {}
 
 module.exports = (req, res, next) => {
-    const __path = req.path
-    const __method = req.method
-    console.log(`Delete me later Path: ${__path}\nMethod: ${__method}`)
+    const path = req.path, method = req.method
+    const property = method === GET ? getPropertyOfUrl(path) : BODY
+    console.log(`Delete me later Path: ${path}\nMethod: ${method}`)
+    const {error, value} = validationSchemas[method][path]?.validate(req[property])
+    logger.info(error, value)
+
+    const valid = error === null
+    if(valid) return next()
+
+    const {details} = error
+    const message = details.map(i => i.message).join(',')
+    logger.info('Error:', message)
+    
+    return res.render('error', {code: 422, msg: message})
+}
 
 
-    next()
+function getPropertyOfUrl(path) {
+    if(QUERY_ROUTES.hasOwnProperty(path)) return QUERY
+    return PARAMS
 }
