dab9da55f95520242f591476881aad92e2a0410d
michaelKurowski <Byamarro@gmail.com>
2021-01-17 18:17:18 +0100

    updated clear-db devops script

--- devops/clear-mongodb.sh
+++ devops/clear-mongodb.sh
@@ -4,6 +4,7 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>clean-mongodb.out 2>&1
 REPO_DIR=$(git rev-parse --show-toplevel)
 cd "${REPO_DIR}/"
-docker-compose down >&3
+docker-compose down mongo >&3
+docker volume rm articleblog_db-data >&3
 rm -rf ./db >&3
-docker-compose up --build >&3
+docker-compose up mongo >&3
