98cee0d436f075a94387790fcdd2869c596fd41e
jordan141 <jmoss636@gmail.com>
2021-01-18 14:17:26 +0000

    fix update schema

--- app/validation/schemas/articles/updateArticleSchema.js
+++ app/validation/schemas/articles/updateArticleSchema.js
@@ -17,6 +17,5 @@ module.exports = Joi.object().keys({
     title: Joi.string().regex(ALPHANUM_DASH_SPACES).min(TITLE_MIN_LENGTH).max(TITLE_MAX_LENGTH).required(),
     description: Joi.string().min(DESC_MIN_LENGTH).max(DESC_MAX_LENGTH).required(),
     body: Joi.string().min(BODY_MIN_LENGTH).max(BODY_MAX_LENGTH).required(),
-    categories: Joi.alternatives(Joi.array().items(Joi.string().valid(...validCategories)), Joi.string().valid(...validCategories), Joi.string().valid(EMPTY_STRING)),
-    header: Joi.string().allow(EMPTY_STRING) //When the header input is empty, the html5 form adds the prop of header onto the request with the value of "", unsure how to stop this behaviour so this will be here until then.
+    categories: Joi.alternatives(Joi.array().items(Joi.string().valid(...validCategories)), Joi.string().valid(...validCategories), Joi.string().valid(EMPTY_STRING))
 })
