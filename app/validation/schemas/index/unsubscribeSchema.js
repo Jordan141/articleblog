9a68cd46b2f9a81a2a89ebebec5640ac6d5e2cf7
jordan141 <jmoss636@gmail.com>
2021-01-18 00:42:00 +0000

    unsubscribe schema

---
+++ app/validation/schemas/index/unsubscribeSchema.js
@@ -0,0 +1,6 @@
+const Joi = require('joi')
+const {EMAIL_MAX_LENGTH, EMAIL_MIN_LENGTH} = require('../../../staticdata/minmax.json').USER
+
+module.exports = Joi.object().keys({
+    email: Joi.string.email().trim().min(EMAIL_MIN_LENGTH).max(EMAIL_MAX_LENGTH).required()
+})
