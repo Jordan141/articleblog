773539c508e59fc9b284d56ce5f0fe2776a9073c
jordan141 <jmoss636@gmail.com>
2021-01-21 12:46:23 +0000

    remove unique requirement on article title

--- app/models/article.js
+++ app/models/article.js
@@ -22,7 +22,7 @@ const articleSchema = new mongoose.Schema({
         type: [{type: String, minlength: TITLE_MIN_LENGTH}],
         default : []
     },
-    title: {type: String, required: true, minlength: TITLE_MIN_LENGTH, maxlength: TITLE_MAX_LENGTH, unique: true},
+    title: {type: String, required: true, minlength: TITLE_MIN_LENGTH, maxlength: TITLE_MAX_LENGTH},
     description: {type: String, required: true, minlength: DESC_MIN_LENGTH, maxlength: DESC_MAX_LENGTH},
     body: {type: String, required: true, minlength: BODY_MIN_LENGTH, maxlength: BODY_MAX_LENGTH},
     isApproved: {type: Boolean, default: false, required: true},
