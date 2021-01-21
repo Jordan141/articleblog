a31d29593ef0f268804131a2a72cfc7efc0b36dd
jordan141 <jmoss636@gmail.com>
2021-01-21 16:18:38 +0000

    typo fix

--- app/models/user.js
+++ app/models/user.js
@@ -47,7 +47,7 @@ async function getLink(link, docType, docId, n = 1) {
     const newLink = n === 1 ? link : `${link}-${n}`
     const doc = await Link.findOne({link: newLink}).exec()
     if(!doc) {
-        Link.create({link, docType, doc_id: docId})
+        Link.create({link: newLink, docType, doc_id: docId})
         return link
     }
     return await getLink(link, docType, docId, n + 1)
